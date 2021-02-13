832140064631b6212af10a27822fef7e9c23c71c
Qulon <biozka2@mail.ru>
2021-02-13 14:03:48 +0000

    module 25 not finished

---
+++ module25/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        port: 3001,
+        host: "0.0.0.0",
+        stats: {
+            children: false,
+            maxModules: 0
+           }
+      },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png)$/,
+                use: {
+                 loader: 'url-loader'
+                }
+            }
+        ]
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         filename: "faq.html",
+    //         template: "./src/faq.html",
+    //         chunks: ['main']
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         filename: "about.html",
+    //         template: "./src/about.html"
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         filename: "register.html",
+    //         template: "./src/register.html"
+    //     }),
+    // ],
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin( {
+            template: "./src/index.html"
+        } )
+    ],
+}
